47993b399dd9a6f3613c3a3dea682f4fc4b96561
mohammed <mohammedadell301@gmail.com>
2022-04-11 05:12:54 +0200

    validation

--- src/app/Components/orders/view-order/view-order.component.ts
+++ src/app/Components/orders/view-order/view-order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Orders } from 'src/app/Models/orders';
+import { OrderService } from 'src/app/Services/order.service';
 
 @Component({
   selector: 'app-view-order',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewOrderComponent implements OnInit {
 
-  constructor() { }
+  OrderID: number=0;
+
+  Order:Orders = {} as Orders;
+
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private router:Router,
+    private OrderService:OrderService
+  ) { }
 
   ngOnInit(): void {
+
+    //to get the specific Order i want to view / update it
+        this.activeRoute.paramMap.subscribe((params)=>{
+          this.OrderID=Number(params.get("oid"));
+          this.OrderService.getOrderByID(this.OrderID).subscribe({
+            next:(data)=>{this.Order=data;
+                          console.log(data);
+                        },
+          });
+        });
   }
+
+  updateOrder(){
+    this.OrderService.updateOrder(this.Order).subscribe({
+      next:(res)=>{
+          alert(" the Order is updated successfully");
+          this.router.navigate(['/orders']);
+          },
+        error:(err)=>{console.log(err)}
+    });
+}
 
 }

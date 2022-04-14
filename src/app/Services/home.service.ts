51036c83041c819250fae21415b3335cc76f2378
sara-ghazy <saraghazy434@gmail.com>
2022-04-14 04:06:23 +0200

    home

---
+++ src/app/Services/home.service.ts
@@ -0,0 +1,25 @@
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  constructor(private httpClient:HttpClient) { }
+
+
+
+  users() {
+  return this.httpClient.get(`${environment.APIBaseURL}/numberusers`);
+   }
+
+   clients()
+   {
+    return this.httpClient.get(`${environment.APIBaseURL}/numbercustomers`);
+
+   }
+
+
+}
